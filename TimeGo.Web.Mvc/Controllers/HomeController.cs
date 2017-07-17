b5f07fb7b051f69290e8b77187342783c948839d
ktairov <pyro.sun@gmail.com>
2017-07-17 09:31:28 +0300

    Added Alba angular4 app and gulp config

--- TimeGo.Web.Mvc/Controllers/HomeController.cs
+++ TimeGo.Web.Mvc/Controllers/HomeController.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TimeGo.Web.Mvc.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            return View("NgApp");
         }
     }
 }
--- TimeGo.Web.Mvc/gulpfile.js
+++ TimeGo.Web.Mvc/gulpfile.js
@@ -1,78 +1,129 @@
-/// <binding BeforeBuild='build' />
-/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-var gulp = require("gulp");
+"use strict";
+
+var gulp = require('gulp');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
-var tsc = require("gulp-typescript");
+var tsc = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-var tsProject = tsc.createProject("tsconfig.json");
+var tsProject = tsc.createProject('tsconfig.json');
 var tslint = require('gulp-tslint');
 
-/// RUN, DEPLOY tasks
-gulp.task('default', ['build', 'dist-libs', 'watch']);
+/**
+ * Build project
+ */
+gulp.task('default', ['build', 'watch']);
 
-gulp.task('clean-libs', function () {
-    return gulp.src('./dist/libs')
+/**
+ * Remove build directory.
+ */
+gulp.task('clean', function () {
+    return gulp.src('./dist/**', { read: false })
         .pipe(clean());
 });
 
-gulp.task("dist-libs", () => {
-    gulp.src([
-            'core-js/client/**',
-            'systemjs/dist/system.src.js',
-            'reflect-metadata/**',
-            'rxjs/**',
-            'zone.js/dist/**',
-            '@angular/**',
-            '@ngrx/**',
-            'deep-freeze-strict/**',
-            'ngrx-store-freeze/**',
-            '@swimlane/ngx-datatable/release/**',
-            'tassign/**',
-            'lodash/**',
-            'jquery/dist/jquery.*js'
-    ], {
-        cwd: "node_modules/**"
-    })
-        .pipe(gulp.dest('./dist/libs'));
+/**
+ * Lint all custom TypeScript files.
+ */
+gulp.task('tslint', () => {
+    return gulp.src("src/**/*.ts")
+        .pipe(tslint({
+            formatter: 'prose'
+        }))
+        .pipe(tslint.report());
 });
 
-var tsProject = ts.createProject('app/tsconfig.json', {
-    typescript: require('typescript')
+/**
+ * Compile TypeScript sources and create sourcemaps in build directory.
+ */
+gulp.task('build-ts', ['tslint'], () => {
+    var tsResult = gulp.src(['./App/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+    return tsResult.js
+        .pipe(sourcemaps.write('.', { sourceRoot: '/App' }))
+        .pipe(gulp.dest('dist/js'));
 });
 
-/// BUILD tasks
-gulp.task('build-ts', function (done) {
-    //var tsResult = tsProject.src()
-    var tsResult = gulp.src([
-            "./app/**/*.ts",
-            "!./app/**/_*.ts"
-    ])
-        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
-    return tsResult.js.pipe(gulp.dest('./dist/js/'));
-});
+/**
+ * Compile SASS sources and create css files.
+ */
 gulp.task('build-sass', function () {
-    gulp.src([
-        './app/**/*.scss',
-        '!./app/**/_*.scss'
-    ])
+    return gulp.src(['./Content/scss/style.scss'])
         .pipe(sass())
-        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./dist/css/'));
 });
-gulp.task('build-html', function () {
-    gulp.src("./app/**/*.html")
-        .pipe(gulp.dest('./dist/js/'))
-});
-gulp.task('build', ['build-ts', 'build-html', 'build-sass']);
 
-/// WATCH tasks
-gulp.task('watch-sass', function () {
-    gulp.watch('./app/**/*.scss', ['build-sass'])
+/**
+ * Copy html views.
+ */
+gulp.task('build-html', function () {
+    return gulp.src("./App/**/*.html")
+        .pipe(gulp.dest('./dist/js/'));
 });
+
+/**
+ * Compile whole app.
+ */
+gulp.task('build', ['build-ts', 'build-html', 'build-sass', 'build-libs']);
+
+/**
+ * Copy all required libraries into build directory.
+ */
+gulp.task('build-libs', () => {
+    gulp.src([
+        'core-js/client/**',
+        'systemjs/dist/system.src.js',
+        'reflect-metadata/**',
+        'rxjs/**',
+        'zone.js/dist/**',
+        'lodash/**',
+        '@angular/**',
+        'angular-calendar/**',
+        'angular2-datatable/**',
+        'angular2-ladda/**',
+        'angular2-text-mask/**',
+        'angular2-toaster/**',
+        'chart.js/**',
+        'dragula/**',
+        'moment/**',
+        'ng2-charts/**',
+        'ng2-dragula/**',
+        'ng2-select/**',
+        'ngx-bootstrap/**',
+        'ts-helpers/**'
+    ], {
+            cwd: 'node_modules/**'
+        }).pipe(gulp.dest('./dist/libs'));
+});
+
+/**
+ * Watch for changes in sass files
+ */
+gulp.task('watch-sass', function () {
+    return gulp.watch('./app/**/*.scss', ['build-sass']);
+});
+
+/**
+ * Watch for changes in ts files
+ */
 gulp.task('watch-ts', ['build-ts'], function () {
     return gulp.watch('app/**/*.ts', ['build-ts']);
 });
+
+/**
+ * Watch for changes
+ */
 gulp.task('watch', ['watch-ts', 'watch-sass']);
+
+
+/**
+ * Run karma unit tests
+ */
+gulp.task('test', function () {
+});
+
+/**
+ * Run e2e tests with protractor
+ */
+gulp.task('e2e', ['build-ts'], function () {
+});
